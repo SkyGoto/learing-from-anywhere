28f09b39cd1a0f29087d3a769fdbd28e6d32d130
SkyGoto <32897640+SkyGoto@users.noreply.github.com>
2017-11-03 20:57:33 +0800

    no

--- sqlconnection.cs
+++ sqlconnection.cs
@@ -23,12 +23,18 @@ namespace test1
         {
             try
             {
+                //判断连接状态
                 if (conn.State == ConnectionState.Open || textBox1.Text != "")
                 {
+                    //创建一个command对象
                     SqlCommand cmd = new SqlCommand();
+                    //选择连接属性
                     cmd.Connection = conn;
+                    //设置命令文本
                     cmd.CommandText = "select count(*) from " + textBox1.Text.Trim();
+                    //设置应用类型
                     cmd.CommandType = CommandType.Text;
+                    //执行select 语句
                     int i = Convert.ToInt32(cmd.ExecuteScalar());
                     label1.Text = "表中共有：" + i.ToString() + "条数据";
                 }
@@ -41,6 +47,7 @@ namespace test1
         SqlConnection conn;
         private void Form1_Load(object sender, EventArgs e)
         {
+            //创建数据库连接对象
             conn = new SqlConnection("server =DESKTOP-SEGMED4;database=db_test;uid=sa;pwd=123456");
             conn.Open();
         }
